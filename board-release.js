55426b1378b3d9b2f949fec3b1029156305f1cb6
James Ketrenos <james.p.ketrenos@intel.com>
2017-10-16 16:50:55 -0700

    board-release.js now functioning and performing full release generation
    Signed-off-by: James Ketrenos <james.p.ketrenos@intel.com>

--- board-release.js
+++ board-release.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
+"use strict";
+
 const board = process.argv[2] ? process.argv[2].toLowerCase() : "",
   child_process = require("child_process"),
   fs = require("fs");
@@ -47,9 +49,11 @@ function execPromise(cmdLine) {
 }
 
 function build(boardDir) {
+  let dir = process.cwd();
+
   return new Promise(function(resolve, reject) {
     try {
-      process.chdir("build/" + boardDir);
+      process.chdir("release/" + boardDir);
     } catch (error) {
       return reject(error);
     }
@@ -60,9 +64,17 @@ function build(boardDir) {
   }).then(function() {
     console.log("Building...");
     return execPromise("polymer build");
-  }).then(function() {
+  }).catch(function(error) {
+    /* On error, chdir back and return the error */
     try {
-      process.chdir("..");
+      process.chdir(dir);
+    } catch (___) {
+      return Promise.reject(error);
+    }
+  }).then(function() {
+    /* On successs, chdir back and return success */
+    try {
+      process.chdir(dir);
     } catch (error) {
       return Promise.reject(error);
     }
@@ -74,10 +86,10 @@ let boardDir = "board-explorer-" + board, releaseVersion = 0,
   files = fs.readdirSync(".");
 
 /* Walk the releases/ directory looking for built versions */
-let regex = new RegExp("^" + boardDir + "([0-9])*\.tgz$");
+let regex = new RegExp("^" + boardDir + "-([0-9])*\.tgz$");
 files.forEach(function(path) {
   let matches = regex.exec(path);
-  if (matches && matches[1] > boardVersion) {
+  if (matches && matches[1] > releaseVersion) {
     releaseVersion = parseInt(matches[1]);
   }
 });
@@ -86,12 +98,12 @@ releaseVersion++;
 boardDir = boardDir + "-" + releaseVersion;
 
 Promise.resolve().then(function() {
-  return execPromise("git checkout-index -a -f --prefix=build/" + boardDir + "/");
+  return execPromise("git checkout-index -a -f --prefix=release/" + boardDir + "/");
 }).then(function() {
   return build(boardDir);
 }).then(function() {
   try {
-    fs.writeFileSync("build/" + boardDir + "/boards.json", JSON.stringify(boards));
+    fs.writeFileSync("release/" + boardDir + "/build/default/boards.json", JSON.stringify(boards));
   } catch (error) {
     console.log(process.cwd());
     return Promise.reject(error);
@@ -99,26 +111,36 @@ Promise.resolve().then(function() {
   return Promise.resolve();
 }).then(function() {
   if (board != "all") {
-    let contents;
+    let content;
     try {
-      content = fs.readFileSync("build/" + boardDir + "/single-board.html");
+      content = fs.readFileSync("release/" + boardDir + "/build/default/index.html", "utf8");
       content = content.replace(
             /<board-explorer-app /g,
-            "<board-explorer single-board='" + board + "' ");
-
-      fs.writeFileSync("build/" + boardDir + "/single-board.html", content);
+            "<board-explorer-app single-board-mode single-board='" + board + "' ");
+      fs.writeFileSync("release/" + boardDir + "/build/default/single-board.html", content);
     } catch (error) {
       return Promise.reject(error);
     }
   }
 }).then(function() {
-  return execPromise("tar czvf " + boardDir + ".tgz -C build " + boardDir);
+  try {
+    fs.renameSync(
+      "release/" + boardDir + "/build/default",
+      "release/" + boardDir + "/build/board-explorer");
+  } catch(error) {
+    return Promise.reject(error);
+  }
+}).then(function() {
+  return execPromise("tar czvf " + boardDir + ".tgz " +
+      "-C release/" + boardDir + "/build board-explorer");
 }).then(function() {
   return execPromise([
       "git tag",
       "-a v" + boardDir,
       "-m 'Tagged-v" + releaseVersion + "-for-" + board + "'"
     ].join(" "));
+}).then(function() {
+  console.log("Completed building:\n  " + boardDir + ".tgz");
 }).catch(function(error) {
   console.error(error);
 });
