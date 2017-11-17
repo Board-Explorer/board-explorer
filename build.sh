edc9b4302274513bbd018007313a378327d36e0c
James Ketrenos <james.p.ketrenos@intel.com>
2017-11-17 10:25:17 -0800

    Updated build.sh
    Signed-off-by: James Ketrenos <james.p.ketrenos@intel.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
-bower install
-polymer build --entrypoint index.html
-mv build build-tmp
-polymer build --entrypoint single-board.html
-rsync -avprl build-tmp/default/ build/default/
-rm -rf build-tmp
+[ "$1" = "-f" ] && {
+  force=1
+} || {
+  force=0
+}
+git status -s -b -u no --porcelain | ( grep -q "^## master" || ((force)) ) || {
+  cat << EOF
+Current branch not master:
 
+  $(git status -s -b -u no --porcelain)
+
+Either checkout master, or specify -f to force build.
+
+EOF
+  exit
+}
+
+([ ! -d bower_components ] || rm -rf bower_components) &&
+bower install &&
+polymer build --entrypoint index.html &&
+mv build build-tmp &&
+polymer build --entrypoint single-board.html &&
+rsync -aprl build-tmp/default/ build/default/ &&
+rm -rf build-tmp && {
+  cat << EOF
+
+Build succeeded. ./publish.sh to push to GitHub.
+
+EOF
+} || {
+  cat << EOF
+
+Build failed.
+
+EOF
+}
--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@
 
 rsync -avprl .git/ build/default/.git/
 cd build/default
-git checkout --orphan gh-pages
+git checkout origin/master --orphan gh-pages
 git add -A .
 git commit -am "seed gh-pages from build system"
 git push -u origin gh-pages --force
